ef8ef3cd6e92daeb518ab9d38b8e5024d570c221
Ítalo Santos <italofsan@outlook.com>
2024-08-14 02:12:44 -0300

    Add react helmet async as a dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import { router } from "./routes.tsx";
 
 import "./global.css";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
