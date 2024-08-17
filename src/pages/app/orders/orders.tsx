1004e14c678d8f5eb2a1ce3b3f14961fad70d917
Ítalo Santos <italofsan@outlook.com>
2024-08-16 22:25:35 -0300

    Feat: Orders page

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,90 @@
+import { Helmet } from "react-helmet-async";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+      <div className="space-y-2.5">
+        <form className="flex items-center gap-2">
+          <span className="text-sm font-semibold">Filtros:</span>
+          <Input placeholder="Nome do cliente " className="h-8 w-[320px]" />
+        </form>
+      </div>
+
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[64px]"></TableHead>
+              <TableHead className="w-[140px]">Identificador</TableHead>
+              <TableHead className="w-[180px]">Realizado há</TableHead>
+              <TableHead className="w-[140px]">Status</TableHead>
+              <TableHead>Cliente</TableHead>
+              <TableHead className="w-[140px]">Total do pedido</TableHead>
+              <TableHead className="w-[164px]"></TableHead>
+              <TableHead className="w-[132px]"></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 10 }).map((_, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Button variant="outline" size="xs">
+                      <Search className="h-3 w-3">
+                        <span className="sr-only">Detalhes do pedido</span>
+                      </Search>
+                    </Button>
+                  </TableCell>
+                  <TableHead className="font-mono text-sm font-medium">
+                    dasdsadasdsd
+                  </TableHead>
+                  <TableHead className="text-muted-foreground">
+                    há 15 minutos
+                  </TableHead>
+                  <TableHead>
+                    <div className="flex items-center gap-2">
+                      <span className="h-2 w-2 rounded-full bg-slate-400" />
+                      <span className="font-medium text-muted-foreground">
+                        Pendente
+                      </span>
+                    </div>
+                  </TableHead>
+                  <TableHead className="font-medium">Ítalo Santos</TableHead>
+                  <TableHead className="font-medium">R$ 149,00</TableHead>
+                  <TableHead>
+                    <Button variant="ghost" size="xs">
+                      <ArrowRight className="mr-2 h-3 w-3" />
+                      Aprovar
+                    </Button>
+                  </TableHead>
+                  <TableHead>
+                    <Button variant="ghost" size="xs">
+                      <X className="mr-2 h-3 w-3" />
+                      Cancelar
+                    </Button>
+                  </TableHead>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { AppLayout } from "./pages/_layouts/app";
 
+import { Orders } from "./pages/app/orders/orders";
 import { Dashboard } from "./pages/app/dashboard";
+
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 
@@ -12,24 +14,16 @@ export const router = createBrowserRouter([
     path: "/",
     element: <AppLayout />,
     children: [
-      {
-        path: "/",
-        element: <Dashboard />,
-      },
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
     ],
   },
   {
     path: "/",
     element: <AuthLayout />,
     children: [
-      {
-        path: "/sign-in",
-        element: <SignIn />,
-      },
-      {
-        path: "/sign-up",
-        element: <SignUp />,
-      },
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> },
     ],
   },
 ]);
